45c49fd5c2c333b03b4678ba5cdaa4da932e85f8
做全栈攻城狮 <iquanzhan@163.com>
2019-12-03 23:19:51 +0800

    初始化

---
+++ vblog-common/src/main/java/com/chengxiaoxiao/common/entity/ErrorMessageConstant.java
@@ -0,0 +1,13 @@
+package com.chengxiaoxiao.common.entity;
+
+/**
+ * @ClassName: ErrorMessageConstant
+ * @description: 错误消息的字段类，保存错误信息
+ * @author: Cheng XiaoXiao  (🍊 ^_^ ^_^)
+ * @Date: 2019-12-01
+ */
+public class ErrorMessageConstant {
+
+    public static final String SUCCESS = "success";
+
+}
